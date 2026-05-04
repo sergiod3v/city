ed09c17453a22bef5ff628bf59f838a4de62a13e
Jackson Camacho <jackson.camacho.hernandez@gmail.com>
2026-05-04 02:03:08 -0500

    fix: open SSH to 0.0.0.0/0 for GH Actions deploy (staging)

--- environments/trading/staging/security_groups.tf
+++ environments/trading/staging/security_groups.tf
@@ -1,16 +1,16 @@
 resource "aws_security_group" "ec2" {
   name        = "behemoth-${var.env}-ec2"
-  description = "Behemoth ${var.env} EC2 - SSH from operator IP only"
+  description = "Behemoth ${var.env} EC2 - SSH open, key-auth only"
   vpc_id      = data.aws_vpc.default.id
 
   tags = { Name = "behemoth-${var.env}-ec2-sg" }
 
   ingress {
-    description = "SSH from operator IP"
+    description = "SSH - key auth enforced"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [var.your_ip_cidr]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
